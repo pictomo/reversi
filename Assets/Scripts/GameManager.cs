06fbfc07abe09a2b3f935ff124e219ee81049716
pictomo <tomo.pict@gmail.com>
2025-07-07 23:55:43 +0900

    [add]board gen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    int[,] board = new int[8, 8];
+    [SerializeField] GameObject square;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                Instantiate(square, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                Debug.Log(board[i, j]);
+            }
+        }
     }
 }
